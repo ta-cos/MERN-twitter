148bdd7b4cec4c1957f49069e5e077ac7c5b2122
Nathan Treadaway <natetread@gmail.com>
2022-12-15 10:07:40 -0500

    Add route /api/users/register, to signup a user encrypt password with bcryptjs and save to DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,37 @@
-const express = require("express");
+const User = require('../../models/User');
+const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.get("/test", (req, res) =>
-res.json({ msg: "This is the users route" }));
+    res.json({ msg: "This is the users route" }));
+
+router.post('/register', async (req, res, next) => {
+    // Check to make sure nobody has already registered with a duplicate email
+    const { handle, email, password } = req.body;
+    const user = await User.findOne({ email })
+    if (user) {
+        // Throw a 400 error if the email address already exists
+        return res.status(400).json({
+            email: "A user has already registered with this email address"
+        })
+    } else {
+        // Otherwise create a new user
+        const newUser = new User({
+            handle,
+            email,
+            password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, async (err, hash) => {
+                if (err) next(err);
+                newUser.password = hash;
+                const user = await newUser.save()
+                return res.json(user)
+            })
+        })
+    }
+})
 
 module.exports = router;
