6492206c5cacb3ae9b206b959158de6649ecd30d
Nathan Treadaway <natetread@gmail.com>
2022-12-15 19:24:55 -0500

    add validation to the register route

--- backend/validation/register.js
+++ backend/validation/register.js
@@ -1,48 +1,54 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
 
-module.exports = function validateRegisterInput(data) {
-    let errors = {};
+module.exports = function validateRegisterInput(req, res, next) {
+    const err = {}
+    err.errors = []
+    const data = {}
 
-    data.handle = validText(data.handle) ? data.handle : '';
-    data.email = validText(data.email) ? data.email : '';
-    data.password = validText(data.password) ? data.password : '';
-    data.password2 = validText(data.password2) ? data.password2 : '';
+    const {handle, email, password, password2} = req.body
+
+    data.handle = validText(handle) ? handle : '';
+    data.email = validText(email) ? email : '';
+    data.password = validText(password) ? password : '';
+    data.password2 = validText(password2) ? password2 : '';
 
     if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
-        errors.handle = 'Handle must be between 2 and 30 characters';
+        err.errors.push('Handle must be between 2 and 30 characters')
     }
 
     if (Validator.isEmpty(data.handle)) {
-        errors.handle = 'Handle field is required';
+        err.errors.push('Handle field is required')
     }
 
     if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
+        err.errors.push('Email field is required')
     }
 
     if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
+        err.errors.push('Email is invalid')
     }
 
     if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
+        err.errors.push('Password field is required')
     }
 
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.password = 'Password must be at least 6 characters';
+        err.errors.push('Password must be at least 6 characters')
     }
 
     if (Validator.isEmpty(data.password2)) {
-        errors.password2 = 'Confirm Password field is required';
+        err.errors.push('Confirm Password field is required')
     }
 
     if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = 'Passwords must match';
+        err.errors.push('Passwords must match')
     }
 
-    return {
-        errors,
-        isValid: Object.keys(errors).length === 0
-    };
+    if (err.errors.length) {
+        err.title = 'Validation Error'
+        err.status = 400
+        next(err)
+    }
+    else next()
 };
